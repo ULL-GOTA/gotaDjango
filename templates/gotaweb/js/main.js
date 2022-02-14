c31ded198d37e5cdb14df92b277da680bc1b3faa
alu0101069937 <alu0101069937@ull.edu.es>
2022-02-14 00:01:20 +0000

    Actualizaciones varias
    - Pruebas con servicio WMS de Copernicus
    - Modificación timeDimension plugin
    - Modificación zoom y posición leyendas
    - Intentando leer alturas de capa

--- templates/gotaweb/js/main.js
+++ templates/gotaweb/js/main.js
@@ -19,7 +19,8 @@
       
       // 2º) A través de Mapbox (previa solicitud token acceso). El id permite varios estilos de mapa.############
       
-	  var outdoors = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiYWx1MDEwMTA2OTkzNyIsImEiOiJjanNvb2hkY3gwbXFyM3lxbHdtY25wZnI2In0.wE4Ct1TZ5cBmcg0QabheJw', { attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a> ', maxZoom: 18, id: 'mapbox.outdoors'});//, accessToken: 'your.mapbox.access.token' .addTo(map);  
+	  var outdoors = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoicHJ1ZWJhbWFwYm94MjIzMiIsImEiOiJja3dtYXR0ZnkyYTFtMm9xdnBoNWtlYmZyIn0.G7xjcWG47ApIc-7HMp6QpA', { attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a> ', maxZoom: 18, id: 'mapbox.satellite'});
+	//, accessToken: 'your.mapbox.access.token' .addTo(map); 
       
       
       //########## Adición de OVERLAYS (manualmente): ##############
@@ -71,8 +72,9 @@
     //##############################################
     //##############################################
     
-    var test_WMS = "https://ogcie.iblsoft.com/metocean/wms";
-    //var test_WMS = "http://10.6.5.230:8080/ncWMS2/wms";
+    //var test_WMS = "https://wms.gota-ull.net:8443/ncWMS/wms";
+    var test_WMS = "https://nrt.cmems-du.eu/thredds/wms/cmems_mod_ibi_phy_anfc_0.027deg-3D_P1D-m";
+    //var test_WMS = "https://ogcie.iblsoft.com/metocean/wms";
     
     //LEER CAPAS AUTOMATICAMENTE DESDE FICHERO XML EN SERVIDOR
     //WMS-SERVER (GetCapabilities)
@@ -129,6 +131,9 @@
 
                 //Dimensión de altura/profundidad (elevationDimension)
 
+		//INTENTAR ESCOGER NODOS dimension="elevation" en una sola instrucción
+		//el problema de leer los valores de altura, es que están todos incluidos en una STRING!!
+
 		dim= layerNodes[i].getElementsByTagName("Dimension");		
 		//alert(layerNodes[i].getElementsByTagName("Dimension").length);
 		n= 0;
@@ -137,7 +142,7 @@
 		while (n < dim.length) {		  		  		
 		  //defaultZ[i]= null;		//condicion inicial
 		  if (dim[n].getAttribute('name')== 'elevation'){
-		    //alert(dim[n].childNodes[0].nodeValue);
+		    //alert(typeof (dim[n].childNodes[0].nodeValue));
 		    ejeZ[ejeZ.length-1]= dim[n].childNodes[0].nodeValue.split(",");
 		    defaultZ[defaultZ.length-1]= (parseInt(dim[n].hasAttribute('default') ? dim[n].getAttribute('default') : 0));
 		    //alert(typeof ejeZ[ejeZ.length-1][1]);
@@ -146,7 +151,7 @@
 		}
 
                 //timeDimension-plugin
-                tdLayer = L.timeDimension.layer.wms(layer, {cache:100, updateTimeDimension: true}); 
+                tdLayer = L.timeDimension.layer.wms(layer, {cache:100, setDefaultTime:true}); 
                                 
                 capas.addBaseLayer(tdLayer,tit);	//Adición de capa al control de capas                                        
 
@@ -164,7 +169,7 @@
   var map = L.map('map', {                        
       //Archipiélago canario:            
       center: [28.25, -15.82],
-      zoom: 4,
+      zoom: 8,
       layers: outdoors, //Capa por defecto.      
       
       // #####  (plugin leaflet.TimeDimension) ##########
@@ -196,7 +201,7 @@
       //"Municipios canarios": municipios,          
       //"PinoCanario": pinar,
       //'temp' : L.timeDimension.layer.wms(T_2m, {cache:50}),      
-      //'TEST_LAYER': L.timeDimension.layer.wms(testLayer, {cache:50, updateTimeDimension: true}),
+      //'TEST_LAYER': L.timeDimension.layer.wms(testLayer, {cache:100, updateTimeDimension: true}),
   };
 
 
@@ -210,7 +215,7 @@
   //####### incluir leyenda ############/
     var leyenda,    
     testLegend = L.control({
-        position: 'bottomright',
+        position: 'bottomleft',
     });
 
    testLegend.onAdd = function(){        
